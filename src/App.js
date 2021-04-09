2575cc6187a0a5ef48854a1bb002da29a84dbc37
hardyyb2 <hardikb3732@gmail.com>
2021-04-09 17:45:25 +0530

    [INITIAL] Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import BackgroundOriginal from "./assets/images/background-original.jpg";
+
+const Card = ({ image, title }) => {
+  return (
+    <div className="Card-root">
+      <div className="Image-container">
+        <img src={image} alt={title} className="Card-img" />
+      </div>
+      <div className="Image-title">{title}</div>
+    </div>
+  );
+};
 
 function App() {
+  const [images, setImages] = useState(null);
+
+  useEffect(() => {
+    const url = `https://picsum.photos/v2/list?limit=100`;
+    axios.get(url).then((res) => {
+      setImages(res.data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <img
+          src={BackgroundOriginal}
+          alt="wooden background"
+          className="Header-img"
+        />
+      </div>
+      <div className="Cards">
+        {images?.map(({ download_url, author }, index) => (
+          <Card image={download_url} title={author} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
